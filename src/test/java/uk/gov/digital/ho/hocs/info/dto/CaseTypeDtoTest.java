888d649b255fdbc264e02edc1a48e83d72fcc730
simonmitchell <simon.mitchell@digital.homeoffice.gov.uk>
2018-10-19 10:13:03 +0100

    started to add unit tests for dtos

---
+++ src/test/java/uk/gov/digital/ho/hocs/info/dto/CaseTypeDtoTest.java
@@ -0,0 +1,22 @@
+package uk.gov.digital.ho.hocs.info.dto;
+
+import org.junit.Test;
+import uk.gov.digital.ho.hocs.info.entities.CaseTypeEntity;
+import uk.gov.digital.ho.hocs.info.entities.CorrespondentType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+public class CaseTypeDtoTest {
+
+    @Test
+    public void shouldBuildCaseTypeDtoFromCaseTypeObject() {
+        CaseTypeEntity caseTypeEntity = new CaseTypeEntity(1,"Name","MIN","DCU");
+
+        CaseTypeDto caseTypeDto = CaseTypeDto.from(caseTypeEntity);
+
+        assertThat(caseTypeDto.getDisplayName()).isEqualTo("Name");
+        assertThat(caseTypeDto.getType()).isEqualTo("MIN");
+
+    }
+}
---
+++ src/test/java/uk/gov/digital/ho/hocs/info/dto/DeadlineDtoTest.java
@@ -0,0 +1,28 @@
+package uk.gov.digital.ho.hocs.info.dto;
+
+import org.junit.Test;
+import uk.gov.digital.ho.hocs.info.entities.Deadline;
+import uk.gov.digital.ho.hocs.info.entities.HolidayDate;
+import uk.gov.digital.ho.hocs.info.entities.Sla;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DeadlineDtoTest {
+
+    @Test
+    public void shouldBuildDeadlineDTOFromDeadlineObject() {
+        Sla sla = new Sla("Draft", 5, "MIN");
+        Set<HolidayDate> holidayDates = new HashSet<HolidayDate>(){{new HolidayDate(1,LocalDate.of(2018,1,1));}};
+        Deadline deadline = new Deadline(LocalDate.of(2018,1,1),sla,holidayDates);
+
+        DeadlineDto deadlineDto = DeadlineDto.from(deadline);
+
+        assertThat(deadlineDto.getDate()).isEqualTo(LocalDate.of(2018,1,8));
+        assertThat(deadlineDto.getType()).isEqualTo("Draft");
+
+    }
+}
