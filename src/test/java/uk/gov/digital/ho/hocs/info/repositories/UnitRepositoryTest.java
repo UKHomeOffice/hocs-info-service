8849e7670a40bc2332c9e8fef805f533d7b27ebf
simonmitchell <simon.mitchell@digital.homeoffice.gov.uk>
2018-12-05 13:39:24 +0000

    added team correctly to group in local-realm for keycloak and added ActiveProfile to repositoryTests

--- src/test/java/uk/gov/digital/ho/hocs/info/repositories/UnitRepositoryTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/info/repositories/UnitRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.digital.ho.hocs.info.entities.CaseTypeEntity;
 import uk.gov.digital.ho.hocs.info.entities.Permission;
@@ -21,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class UnitRepositoryTest {
 
 
