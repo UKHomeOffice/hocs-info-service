52e082e79a6fb36105843d7c1433f596739c3976
Stuart Whitlam <stuart.whitlam@digital.homeoffice.gov.uk>
2018-12-17 12:13:28 +0000

    add case type to stagetypes table to enable deadlines to be retrieved by case type and stage type

--- src/main/resources/db/postgresql/V1_4__CREATE_TABLE_STAGE_TYPE.sql
+++ src/main/resources/db/postgresql/V1_4__CREATE_TABLE_STAGE_TYPE.sql
@@ -9,17 +9,19 @@ CREATE TABLE IF NOT EXISTS stage_type
   team_uuid    UUID    NOT NULL,
   deadline     INT     NOT NULL,
   active       boolean NOT NULL,
+  case_type    TEXT    NOT NULL,
 
   CONSTRAINT stage_type_type_idempotent UNIQUE ( type ),
   CONSTRAINT stage_type_short_idempotent UNIQUE (short_code),
   CONSTRAINT fk_team_uuid FOREIGN KEY (team_uuid) REFERENCES team (uuid)
   );
 
+
 CREATE INDEX idx_stage_type_active
 ON stage_type (active);
 
 
 CREATE OR REPLACE VIEW sla AS
-SELECT id as id, type as stage_type, deadline as value, team_uuid as case_type
+SELECT id as id, type as stage_type, deadline as value, case_type
 FROM stage_type
 WHERE deadline <> 0;
