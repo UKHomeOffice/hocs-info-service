6f5cd51563a8ebcf2ff6f9d948a8422a32f471c8
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-05 12:04:47 +0100

    Integration issues between case and info

--- src/main/java/uk/gov/digital/ho/hocs/info/api/TemplateResource.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/api/TemplateResource.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uk.gov.digital.ho.hocs.info.api.dto.CreateTemplateDocumentDto;
-import uk.gov.digital.ho.hocs.info.api.dto.GetStandardLineResponse;
 import uk.gov.digital.ho.hocs.info.api.dto.GetTemplateResponse;
 import uk.gov.digital.ho.hocs.info.domain.model.Template;
 
 import java.util.Set;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
@@ -24,13 +22,13 @@ public class TemplateResource {
         this.templateService = templateService;
     }
 
-    @GetMapping(value = "/caseType/{caseType}/template", produces = APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(value = "/template", produces = APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<Set<GetTemplateResponse>> getTemplates() {
         Set<Template> templates = templateService.getActiveTemplates();
         return ResponseEntity.ok(templates.stream().map(GetTemplateResponse::from).collect(Collectors.toSet()));
     }
 
-    @GetMapping(value = "/template", produces = APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(value = "/caseType/{caseType}/template", produces = APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<GetTemplateResponse> getTemplatesForCaseType(@PathVariable String caseType) {
         Template template = templateService.getTemplateForCaseType(caseType);
         return ResponseEntity.ok(GetTemplateResponse.from(template));
