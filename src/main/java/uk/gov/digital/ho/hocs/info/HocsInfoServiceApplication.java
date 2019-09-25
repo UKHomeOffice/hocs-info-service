b36bfe9c3138b235c22a18b4dc2c6ac7eb81129e
Lewis Muirhead <53258753+lewismuirhead@users.noreply.github.com>
2019-09-25 17:48:17 +0100

    HOCS-1096-all-parent-topics-endpoint - remove a formatting issue

--- src/main/java/uk/gov/digital/ho/hocs/info/HocsInfoServiceApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/HocsInfoServiceApplication.java
@@ -1,12 +1,12 @@
 package uk.gov.digital.ho.hocs.info;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.https://github.com/UKHomeOffice/hocs-info-service/network/alertsautoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HocsInfoServiceApplication {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(HocsInfoServiceApplication.class, args);
     }
 }
