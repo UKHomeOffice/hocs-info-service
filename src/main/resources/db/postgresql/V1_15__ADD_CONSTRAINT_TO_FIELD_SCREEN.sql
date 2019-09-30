17121e7454bb63989a818994860e2bef29688267
Mark Wright <mark.wright@bjss.com>
2019-09-30 16:08:11 +0100

    Add unique constraint to case_type_schema table.

--- src/main/resources/db/postgresql/V1_15__ADD_CONSTRAINT_TO_FIELD_SCREEN.sql
+++ src/main/resources/db/postgresql/V1_15__ADD_CONSTRAINT_TO_FIELD_SCREEN.sql
@@ -1,3 +1,9 @@
 ALTER TABLE field_screen
     ADD CONSTRAINT field_screen_unique_schema_field
-    UNIQUE (schema_uuid, field_uuid);
+    UNIQUE (schema_uuid,field_uuid);
+
+
+ALTER TABLE case_type_schema
+    ADD CONSTRAINT case_type_schema_unique_schema_case_stage
+    UNIQUE (schema_uuid,case_type,stage_type);
+
