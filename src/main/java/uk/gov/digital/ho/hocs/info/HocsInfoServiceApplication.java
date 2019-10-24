54161e05db75080031ba933acbaef5db0ecbc6ea
Paul Rau <paul.rau1@digital.homeoffice.gov.uk>
2019-10-24 14:07:09 +0100

    HOCS-1061 This is very annoying. No reason for the connection failure and no trace on the values used for the connection. Inserting debugs for the application properties for the connections.

--- src/main/java/uk/gov/digital/ho/hocs/info/HocsInfoServiceApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/HocsInfoServiceApplication.java
@@ -1,12 +1,30 @@
 package uk.gov.digital.ho.hocs.info;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+
+@Slf4j
 @SpringBootApplication
 public class HocsInfoServiceApplication {
 
     public static void main(String[] args) {
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileReader("./application.properties"));
+            log.debug("Application Properties:");
+            log.debug("==============================");
+            for (Map.Entry entry :  properties.entrySet()) {
+                log.debug(entry.getKey() + " : " + entry.getValue());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         SpringApplication.run(HocsInfoServiceApplication.class, args);
     }
 }
