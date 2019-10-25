39c7064717118b73a685bf4e5b507a4629ac2979
Paul Rau <paul.rau1@digital.homeoffice.gov.uk>
2019-10-25 09:30:00 +0100

    HOCS-1061 Removing the logger debug and trace settings.

--- src/main/java/uk/gov/digital/ho/hocs/info/HocsInfoServiceApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/HocsInfoServiceApplication.java
@@ -1,32 +1,12 @@
 package uk.gov.digital.ho.hocs.info;
 
-import lombok.extern.slf4j.Slf4j;
-import org.hibernate.cfg.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Map;
-import java.util.Properties;
-
-@Slf4j
 @SpringBootApplication
 public class HocsInfoServiceApplication {
 
     public static void main(String[] args) {
-
-        log.debug("System Properties:");
-        log.debug("==============================");
-        Properties properties = System.getProperties();
-        for (Map.Entry entry : properties.entrySet()) {
-            log.debug(entry.getKey() + " : " + entry.getValue());
-        }
-        log.debug("Environment Properties:");
-        log.debug("==============================");
-        properties = Environment.getProperties();
-        for (Map.Entry entry : properties.entrySet()) {
-            log.debug(entry.getKey() + " : " + entry.getValue());
-        }
-
         SpringApplication.run(HocsInfoServiceApplication.class, args);
     }
 }
