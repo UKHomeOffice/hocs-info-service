c1d6e0805c84a1fcf738f05e3efc728bcfd97f7e
hubert.raich <hubert.raich@bjss.com>
2019-11-04 11:36:27 +0000

    HOCS-1220 - Work stack changes for WCS

---
+++ src/test/java/uk/gov/digital/ho/hocs/info/api/dto/WorkstackColumnDtoTest.java
@@ -0,0 +1,32 @@
+package uk.gov.digital.ho.hocs.info.api.dto;
+
+import org.junit.Assert;
+import org.junit.Test;
+import uk.gov.digital.ho.hocs.info.domain.model.WorkstackColumn;
+
+
+public class WorkstackColumnDtoTest {
+
+    @Test
+    public void from() {
+        String parentSystemName = "system";
+        String displayName = "ColumnABC";
+        String dataAdapter = "adapter123";
+        String renderer = "renderer";
+        String dataValueKey = "valueKey";
+        Boolean filterable = true;
+        String headerClassName = "some css header name";
+        WorkstackColumn workstackColumn = new WorkstackColumn(10L, parentSystemName, displayName, dataAdapter, renderer, dataValueKey, filterable, headerClassName);
+
+        WorkstackColumnDto dto = WorkstackColumnDto.from(workstackColumn);
+
+        Assert.assertEquals("Workstack column display name do not match", displayName, dto.getDisplayName());
+        Assert.assertEquals("Workstack column data adapter do not match", dataAdapter, dto.getDataAdapter());
+        Assert.assertEquals("Workstack column renderer do not match", renderer, dto.getRenderer());
+        Assert.assertEquals("Workstack column data value key do not match", dataValueKey, dto.getDataValueKey());
+        Assert.assertEquals("Workstack column isFilterable do not match", filterable, dto.isFilterable());
+        Assert.assertEquals("Workstack column header class name do not match", headerClassName, dto.getHeaderClassName());
+
+
+    }
+}
