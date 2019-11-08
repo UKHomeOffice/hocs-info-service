4318f8e346b3023ce511e874698731e852ea63c9
hubert.raich <hubert.raich@bjss.com>
2019-11-08 10:20:36 +0000

    HOCS-1260 - Back button within mark up stage

---
+++ src/test/java/uk/gov/digital/ho/hocs/info/api/dto/SecondaryActionDtoTest.java
@@ -0,0 +1,36 @@
+package uk.gov.digital.ho.hocs.info.api.dto;
+
+import org.junit.Test;
+import uk.gov.digital.ho.hocs.info.domain.model.SecondaryAction;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SecondaryActionDtoTest {
+
+    @Test
+    public void from() {
+
+        Long id = 11l;
+        UUID uuid = UUID.randomUUID();
+        String component = "Component";
+        String name = "Name";
+        String label = "Label";
+        String validation = "Validation";
+        String props = "props";
+
+
+        SecondaryAction secondaryAction = new SecondaryAction(id, uuid, component, name, label, validation, props);
+
+        SecondaryActionDto result = SecondaryActionDto.from(secondaryAction);
+
+        assertThat(result.getUuid()).isEqualTo(uuid);
+        assertThat(result.getComponent()).isEqualTo(component);
+        assertThat(result.getName()).isEqualTo(name);
+        assertThat(result.getLabel()).isEqualTo(label);
+        assertThat(result.getValidation()).isEqualTo(validation);
+        assertThat(result.getProps()).isEqualTo(props);
+
+    }
+}
