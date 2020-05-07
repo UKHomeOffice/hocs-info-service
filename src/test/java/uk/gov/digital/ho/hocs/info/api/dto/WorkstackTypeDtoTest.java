041bfb7e8be31dae9fab663f5c9dfb319eb248e3
Parth Vyas <parth.vyas@digital.homeoffice.gov.uk>
2020-05-07 15:43:58 +0100

    HOCS-1695: Modify WorkstackTypeDtoTest.java to test workstackColumns property.

--- src/test/java/uk/gov/digital/ho/hocs/info/api/dto/WorkstackTypeDtoTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/info/api/dto/WorkstackTypeDtoTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import uk.gov.digital.ho.hocs.info.domain.model.WorkstackColumn;
 import uk.gov.digital.ho.hocs.info.domain.model.WorkstackType;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,14 +14,26 @@ public class WorkstackTypeDtoTest {
     @Test
     public void from() {
 
-        List<WorkstackColumn> workstackColumns = Arrays.asList(new WorkstackColumn(10L, "columnName1", "adapter", "renderer", "valueKey", true, "cssClass"));
+        String displayName = "columnName1";
+        String dataAdapter = "adapter";
+        String renderer = "some_renderer";
+        String dataValueKey = "some_valueKey";
+        Boolean isFilterable = true;
+        String cssClass = "some_css_class";
+
+        List<WorkstackColumn> workstackColumns = Arrays.asList(new WorkstackColumn(10L, displayName, dataAdapter, renderer, dataValueKey, isFilterable, cssClass));
         WorkstackType workstackType = new WorkstackType(10L, "system", "some_type", workstackColumns);
 
 
         WorkstackTypeDto sut = WorkstackTypeDto.from(workstackType);
 
         Assert.assertEquals("Type value returns expected value", "some_type", sut.getType());
-        Assert.assertEquals("workstackColumn contains expected ArrayList object", ArrayList.class, sut.getWorkstackColumns().getClass());
+        Assert.assertEquals("workstackColumn displayName contains expected string", displayName, sut.getWorkstackColumns().get(0).getDisplayName());
+        Assert.assertEquals("workstackColumn dataAdapter contains expected string", dataAdapter, sut.getWorkstackColumns().get(0).getDataAdapter());
+        Assert.assertEquals("workstackColumn renderer contains expected string", renderer, sut.getWorkstackColumns().get(0).getRenderer());
+        Assert.assertEquals("workstackColumn dataValueKey contains expected string", dataValueKey, sut.getWorkstackColumns().get(0).getDataValueKey());
+        Assert.assertEquals("workstackColumn isFilterable contains expected boolean", isFilterable, sut.getWorkstackColumns().get(0).isFilterable());
+        Assert.assertEquals("workstackColumn headerClassName contains expected string", cssClass, sut.getWorkstackColumns().get(0).getHeaderClassName());
 
     }
 }
