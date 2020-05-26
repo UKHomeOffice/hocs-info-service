6955a10e1caa5426cbd0c1f6055281d388f52e22
Paul Rau <paul.rau1@digital.homeoffice.gov.uk>
2020-05-26 08:53:23 +0100

    HOCS-1751 Updating CI scripts with new tokens for the HOCS service separation. We no longer have everything under CS. They are separated into namespaces.

--- deploy.sh
+++ deploy.sh
@@ -7,31 +7,40 @@ if [[ -z ${VERSION} ]] ; then
     export VERSION=${IMAGE_VERSION}
 fi
 
-if [[ ${ENVIRONMENT} == "prod" ]] ; then
-    echo "deploy ${VERSION} to prod namespace, using HOCS_INFO_SERVICE_PROD drone secret"
-    export KUBE_TOKEN=${HOCS_INFO_SERVICE_PROD}
+if [[ ${KUBE_NAMESPACE} == "cs-prod" ]] ; then
+    echo "deploy ${VERSION} to prod namespace, using HOCS_INFO_SERVICE_PROD_CS drone secret"
+    export KUBE_TOKEN=${HOCS_INFO_SERVICE_PROD_CS}
     export REPLICAS="2"
+elif [[ ${KUBE_NAMESPACE} == "wcs-prod" ]] ; then
+    echo "deploy ${VERSION} to prod namespace, using HOCS_INFO_SERVICE_PROD_WCS drone secret"
+    export KUBE_TOKEN=${HOCS_INFO_SERVICE_PROD_WCS}
+    export REPLICAS="2"
+elif [[ ${KUBE_NAMESPACE} == "cs-qa" ]] ; then
+    echo "deploy ${VERSION} to test namespace, using HOCS_INFO_SERVICE_QA_CS drone secret"
+    export KUBE_TOKEN=${HOCS_INFO_SERVICE_QA_CS}
+    export REPLICAS="2"
+elif [[ ${KUBE_NAMESPACE} == "wcs-qa" ]] ; then
+    echo "deploy ${VERSION} to test namespace, using HOCS_INFO_SERVICE_QA_WCS drone secret"
+    export KUBE_TOKEN=${HOCS_INFO_SERVICE_QA_WCS}
+    export REPLICAS="2"
+elif [[ ${KUBE_NAMESPACE} == "cs-demo" ]] ; then
+    echo "deploy ${VERSION} to demo namespace, using HOCS_INFO_SERVICE_DEMO_CS drone secret"
+    export KUBE_TOKEN=${HOCS_INFO_SERVICE_DEMO_CS}
+    export REPLICAS="1"
+elif  [[ ${KUBE_NAMESPACE} == "wcs-demo" ]] ; then
+    echo "deploy ${VERSION} to demo namespace, using HOCS_INFO_SERVICE_DEMO_WCS drone secret"
+    export KUBE_TOKEN=${HOCS_INFO_SERVICE_DEMO_WCS}
+    export REPLICAS="1"
+elif [[ ${KUBE_NAMESPACE} == "cs-dev" ]] ; then
+    echo "deploy ${VERSION} to dev namespace, using HOCS_INFO_SERVICE_DEV_CS drone secret"
+    export KUBE_TOKEN=${HOCS_INFO_SERVICE_DEV_CS}
+    export REPLICAS="1"
+elif [[ ${KUBE_NAMESPACE} == "wcs-dev" ]] ; then
+    echo "deploy ${VERSION} to dev namespace, using HOCS_INFO_SERVICE_DEV_WCS drone secret"
+    export KUBE_TOKEN=${HOCS_INFO_SERVICE_DEV_WCS}
+    export REPLICAS="1"
 else
-    if [[ ${ENVIRONMENT} == "qa" ]] ; then
-        echo "deploy ${VERSION} to test namespace, using HOCS_INFO_SERVICE_QA drone secret"
-        export KUBE_TOKEN=${HOCS_INFO_SERVICE_QA}
-        export REPLICAS="2"
-    elif [[ ${ENVIRONMENT} == "demo" ]] ; then
-        echo "deploy ${VERSION} to demo namespace, using HOCS_INFO_SERVICE_DEMO drone secret"
-        export KUBE_TOKEN=${HOCS_INFO_SERVICE_DEMO}
-        export REPLICAS="1"
-    elif [[ ${KUBE_NAMESPACE} == "cs-dev" ]] ; then
-        echo "deploy ${VERSION} to dev namespace, using HOCS_INFO_SERVICE_DEV drone secret"
-        export KUBE_TOKEN=${HOCS_INFO_SERVICE_DEV_CS}
-        export REPLICAS="1"
-    elif [[ ${KUBE_NAMESPACE} == "wcs-dev" ]] ; then
-        echo "deploy ${VERSION} to dev namespace, using HOCS_INFO_SERVICE_DEV drone secret"
-        export KUBE_TOKEN=${HOCS_INFO_SERVICE_DEV_WCS}
-        export REPLICAS="1"
-    else
-        echo "Unable to find environment: ${ENVIRONMENT}"        
-    fi
-
+    echo "Unable to find environment: ${ENVIRONMENT}"
 fi
 
 if [[ -z ${KUBE_TOKEN} ]] ; then
