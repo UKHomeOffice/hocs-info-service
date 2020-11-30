2f777dab071148ea4db42f6b2cc8dd5a37e47dae
Mark Wright <46746088+MarkWrightBjss@users.noreply.github.com>
2020-11-30 12:10:36 +0000

    HOCS-2214 : Multi.Contrib. endpoints for returning Somu Types (#243)
    * HOCS-2214 : Multi.Contrib. endpoints for returning Somu Types
    
    * HOCS-2214 : Multi.Contrib. endpoints for returning Somu Types, repo tests
    
    * HOCS-2214 : Multi.Contrib. endpoints for returning Somu Types, repo tests
    
    * HOCS-2214 : Multi.Contrib. endpoints for returning Somu Types, repo tests
    
    * HOCS-2214 : Multi.Contrib. endpoints for returning Somu Types, repo tests
    
    * HOCS-2214 : Multi.Contrib. endpoints for Somu Types, code review
    
    * HOCS-2214 : Multi.Contrib. endpoints for Somu Types, code review
    
    * HOCS-2214 : Multi.Contrib. endpoints for Somu Types, deletion code review

---
+++ src/test/java/uk/gov/digital/ho/hocs/info/api/SomuTypeResourceTest.java
@@ -0,0 +1,94 @@
+package uk.gov.digital.ho.hocs.info.api;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+import uk.gov.digital.ho.hocs.info.api.dto.CreateSomuTypeDto;
+import uk.gov.digital.ho.hocs.info.api.dto.SomuTypeDto;
+import uk.gov.digital.ho.hocs.info.domain.model.SomuType;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SomuTypeResourceTest {
+
+    @Mock
+    SomuTypeService somuTypeService;
+
+    SomuTypeResource somuTypeResource;
+
+    @Before
+    public void setup() {
+        somuTypeResource = new SomuTypeResource(somuTypeService);
+    }
+
+    @Test
+    public void getAllSomuTypes() {
+        SomuType somuType = new SomuType(1L, UUID.randomUUID(), "CaseType", "Type", "{}", true);
+        Set<SomuType> somuTypes = new HashSet<SomuType>() {{
+            add(somuType);
+        }};
+        when(somuTypeService.getAllSomuTypes()).thenReturn(somuTypes);
+
+        ResponseEntity<Set<SomuTypeDto>> result = somuTypeResource.getAllSomuTypes();
+
+        assertThat(result).isNotNull();
+        assertThat(result.getStatusCodeValue()).isEqualTo(200);
+        assertThat(result.getBody()).isNotNull();
+        assertThat(result.getBody().size()).isEqualTo(1);
+        SomuTypeDto somuTypeDto = (SomuTypeDto) result.getBody().toArray()[0];
+        assertThat(somuTypeDto.getCaseType()).isEqualTo("CaseType");
+        assertThat(somuTypeDto.getType()).isEqualTo("Type");
+        assertThat(somuTypeDto.getSchema()).isEqualTo("{}");
+        assertThat(somuTypeDto.isActive()).isTrue();
+        verify(somuTypeService).getAllSomuTypes();
+        verifyNoMoreInteractions(somuTypeService);
+    }
+
+    @Test
+    public void getSomuTypeForCaseTypeAndType() {
+        SomuType somuType = new SomuType(1L, UUID.randomUUID(), "CaseType", "Type", "{}", true);
+        when(somuTypeService.getSomuTypeForCaseTypeAndType("CaseType", "Type")).thenReturn(somuType);
+
+        ResponseEntity<SomuTypeDto> result = somuTypeResource.getSomuTypeForCaseTypeAndType("CaseType", "Type");
+
+        assertThat(result).isNotNull();
+        assertThat(result.getStatusCodeValue()).isEqualTo(200);
+        assertThat(result.getBody()).isNotNull();
+        SomuTypeDto somuTypeDto = result.getBody();
+        assertThat(somuTypeDto.getCaseType()).isEqualTo("CaseType");
+        assertThat(somuTypeDto.getType()).isEqualTo("Type");
+        assertThat(somuTypeDto.getSchema()).isEqualTo("{}");
+        assertThat(somuTypeDto.isActive()).isTrue();
+        verify(somuTypeService).getSomuTypeForCaseTypeAndType("CaseType", "Type");
+        verifyNoMoreInteractions(somuTypeService);
+    }
+
+    @Test
+    public void upsertSomuTypeForCaseTypeAndType() {
+        SomuType somuType = new SomuType(1L, UUID.randomUUID(), "CaseType", "Type", "{}", true);
+        when(somuTypeService.upsertSomuTypeForCaseTypeAndType("CaseType", "Type", "{}", true)).thenReturn(somuType);
+        CreateSomuTypeDto createSomuTypeDto = new CreateSomuTypeDto("CaseType", "Type", "{}", true);
+
+        ResponseEntity<SomuTypeDto> result = somuTypeResource.upsertSomuTypeForCaseTypeAndType(createSomuTypeDto);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getStatusCodeValue()).isEqualTo(200);
+        assertThat(result.getBody()).isNotNull();
+        SomuTypeDto somuTypeDto = result.getBody();
+        assertThat(somuTypeDto.getCaseType()).isEqualTo("CaseType");
+        assertThat(somuTypeDto.getType()).isEqualTo("Type");
+        assertThat(somuTypeDto.getSchema()).isEqualTo("{}");
+        assertThat(somuTypeDto.isActive()).isTrue();
+        verify(somuTypeService).upsertSomuTypeForCaseTypeAndType("CaseType", "Type", "{}", true);
+        verifyNoMoreInteractions(somuTypeService);
+    }
+}
