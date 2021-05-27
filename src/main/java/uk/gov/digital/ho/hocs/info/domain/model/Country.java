ff11625cd74c9f698d1982c7ebd751617d4367dc
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-05-27 15:13:16 +0100

    Feature/hocs 3031 nationality list (#296)
    * HOCS-3031 - Fix country list refresh.
    
    * HOCS-3031 - Update test properties
    
    * HOCS-3031 - Fix lombok warnings
    
    * HOCS-3031 - Refresh countries on startup

--- src/main/java/uk/gov/digital/ho/hocs/info/domain/model/Country.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/domain/model/Country.java
@@ -1,9 +1,6 @@
 package uk.gov.digital.ho.hocs.info.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -13,6 +10,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class Country implements Serializable {
     @Id
     @Column(name = "id")
