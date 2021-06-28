9f2a7d030f26c5a8f272cd122d592aec25452d5d
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-06-28 14:43:19 +0100

    Feature/hocs 3290 exec java (#308)
    * HOCS-3290 - add exec to java start script
    
    * HOCS-3290 - Add shutdown logging

--- src/main/java/uk/gov/digital/ho/hocs/info/HocsInfoServiceApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/HocsInfoServiceApplication.java
@@ -1,9 +1,13 @@
 package uk.gov.digital.ho.hocs.info;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
+import javax.annotation.PreDestroy;
+
+@Slf4j
 @SpringBootApplication
 @EnableRetry
 public class HocsInfoServiceApplication {
@@ -11,4 +15,10 @@ public class HocsInfoServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(HocsInfoServiceApplication.class, args);
     }
+
+    @PreDestroy
+    public void stop() {
+        log.info("hocs-info-service stopping gracefully");
+    }
+
 }
