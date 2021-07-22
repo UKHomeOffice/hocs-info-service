861e823e005f5b733e97a4b55a6a61efef2f2caf
agraham19 <35735033+agraham19@users.noreply.github.com>
2021-07-22 14:20:08 +0100

    HOCS-3287 Check your answers screen (#315)
    * Allow fields to compose other fields
    * Add integration tests for schema

--- src/test/java/uk/gov/digital/ho/hocs/info/api/SchemaResourceTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/info/api/SchemaResourceTest.java
@@ -32,7 +32,8 @@ public class SchemaResourceTest {
 
     @Test
     public void shouldGetAllSummaryFieldsForCaseType() {
-        Field field = new Field("component", "Field1", "label", "", "", true);
+        Field childField = new Field("component", "childField", "label", "", "", true, null);
+        Field field = new Field("component", "Field1", "label", "", "", true, childField);
         List<Field> fields = new ArrayList<>();
         fields.add(field);
         when(schemaService.getAllSummaryFieldsForCaseType("CASE_TYPE")).thenReturn(fields);
@@ -41,6 +42,7 @@ public class SchemaResourceTest {
         assertThat(result.getBody()).isNotNull();
         assertThat(result.getBody().size()).isEqualTo(1);
         assertThat(result.getBody().get(0).getName()).isEqualTo("Field1");
+        assertThat(result.getBody().get(0).getChild().getUuid()).isEqualTo(childField.getUuid());
 
     }
 
