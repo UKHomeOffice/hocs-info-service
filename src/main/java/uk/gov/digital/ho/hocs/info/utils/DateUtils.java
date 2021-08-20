c13297a95948bdb93bf22d27fd3ca7ef78ef21a7
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-08-20 09:34:31 +0100

    HOCS-3631: case deadline dates (#323)
    * HOCS-3631: change date utils
    
    As the only current use case for the DateUtils class is to check if a
    date is a non-working day, this change pulls that logic into this file.
    As a result the isWeekend method can be made private.
    
    * HOCS-3631: change deadline implementation
    
    If a date was added on a weekend then the counting would start
    from that date. This change moves this date forward to the next 
    non-working date before the calculation is started.
    
    * HOCS-3631: use new DateUtil
    
    The CaseTypeService currently uses the old isWeekend method, 
    as a simplified method has been added that checks if a date is a 
    non-working day, this is now used.

--- src/main/java/uk/gov/digital/ho/hocs/info/utils/DateUtils.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/utils/DateUtils.java
@@ -2,13 +2,19 @@ package uk.gov.digital.ho.hocs.info.utils;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Set;
 
 public final class DateUtils {
 
-    public static boolean isWeekend(LocalDate date) {
-        if (date == null) {
+    private static boolean isWeekend(LocalDate date) {
+        return date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY;
+    }
+
+    public static boolean isDateNonWorkingDay(LocalDate date, Set<LocalDate> holidayDates) {
+        if (date == null || holidayDates == null) {
             return false;
         }
-        return date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY;
+
+        return (DateUtils.isWeekend(date) || holidayDates.contains(date));
     }
 }
