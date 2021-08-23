120bb08a38335598b69fb6d56a3129d1f187a186
fewtrees-ho <robert.smallwood@digital.homeoffice.gov.uk>
2021-08-23 17:07:20 +0100

    HOCS-3556 - TEMP Add constraint to retrieving caseTypes

--- src/main/java/uk/gov/digital/ho/hocs/info/domain/repository/CaseTypeRepository.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/domain/repository/CaseTypeRepository.java
@@ -10,13 +10,15 @@ import java.util.Set;
 @Repository
 public interface CaseTypeRepository extends CrudRepository<CaseType, String> {
 
-    @Query(value = "SELECT ct.* FROM case_type ct JOIN permission ON ct.type = permission.case_type WHERE permission.team_uuid IN ?1 AND ct.active = TRUE", nativeQuery = true)
+    @Query(value = "SELECT ct.* FROM case_type ct JOIN permission ON ct.type = permission.case_type WHERE permission.team_uuid IN ?1 AND ct.active AND ct.previous_case_type IS NULL", nativeQuery = true)
     Set<CaseType> findAllCaseTypesByTeam(Set<String> team);
 
-    @Query(value = "SELECT ct.* FROM case_type ct JOIN permission ON ct.type = permission.case_type WHERE permission.team_uuid IN ?1 AND ct.active = TRUE AND ct.bulk = TRUE", nativeQuery = true)
+    @Query(value = "SELECT ct.* FROM case_type ct JOIN permission ON ct.type = permission.case_type WHERE permission.team_uuid IN ?1 AND ct.active AND ct.bulk AND ct.previous_case_type IS NULL", nativeQuery = true)
     Set<CaseType> findAllBulkCaseTypesByTeam(Set<String> team);
 
     Set<CaseType> findAll();
+
+    Set<CaseType> findByPreviousCaseTypeIsNull();
 
     CaseType findByShortCode(String shortCode);
 
