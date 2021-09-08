7c9155457102d83bee5e1bb7ec65f517baf5242d
RobSmallwood <robert.smallwood@digital.homeoffice.gov.uk>
2021-09-08 09:11:25 +0100

    HOCS-3556 - Remove casetype update statement for COMP2

--- src/main/resources/db/postgresql/V1_48__3556_AddCaseType.sql
+++ src/main/resources/db/postgresql/V1_48__3556_AddCaseType.sql
@@ -1,5 +1,3 @@
 ------------------------------------------------------------------------
 alter table case_type add "previous_case_type" text;
 
--- Update Stage 2 case and specify COMP case type predecessor
-update case_type set previous_case_type = 'COMP' where uuid = '51463200-e01d-4573-87b5-514632000000';
