5bafe793802b79498685cb9cc10b60f78e6381cd
LaurieBennett <laurie.bennett@digital.homeoffice.gov.uk>
2022-01-20 14:31:25 +0000

    HOCS-4450 added units to DTO and changed teams to pass through names

--- src/main/java/uk/gov/digital/ho/hocs/info/api/dto/UserWithTeamsDto.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/api/dto/UserWithTeamsDto.java
@@ -17,10 +17,17 @@ public class UserWithTeamsDto {
     private String firstName;
     private String lastName;
     private List<String> teamNames;
+<<<<<<< HEAD
     private Map<String, String> unitNames;
     private boolean enabled;
 
     public static UserWithTeamsDto from(UserDto user, List<String> teamNames, Map<String, String> unitNames) {
+=======
+    private List<String> unitNames;
+    private boolean enabled;
+
+    public static UserWithTeamsDto from(UserDto user, List<String> teamNames, List<String> unitNames) {
+>>>>>>> 44c4cae (HOCS-4450 added units to DTO and changed teams to pass through names)
         return new UserWithTeamsDto(user.getId(), user.getUsername(), user.getEmail(), user.getFirstName(), user.getLastName(), teamNames, unitNames, user.isEnabled());
     }
 }
