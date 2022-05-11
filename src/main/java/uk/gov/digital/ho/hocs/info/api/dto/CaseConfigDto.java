ffb0e8591ac3d5bb2ec776ae5d9272393835757c
Joseph Rogerson <92527240+JosephRogerson@users.noreply.github.com>
2022-05-11 14:44:59 +0100

    Update src/main/java/uk/gov/digital/ho/hocs/info/api/dto/CaseConfigDto.java
    Co-authored-by: Robert Deniszczyc <72561986+robertdeniszczyc2@users.noreply.github.com>

--- src/main/java/uk/gov/digital/ho/hocs/info/api/dto/CaseConfigDto.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/api/dto/CaseConfigDto.java
@@ -1,6 +1,5 @@
 package uk.gov.digital.ho.hocs.info.api.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
