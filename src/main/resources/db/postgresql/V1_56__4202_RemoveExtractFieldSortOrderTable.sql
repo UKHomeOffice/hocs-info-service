8785e8318c50c48184eb306a1d9e329cf1d562bc
Liam MacPherson <liammacpherson1996@gmail.com>
2022-05-27 11:02:15 +0100

    HOCS-4202: remove unused table reference
    There is currently an unused table `extract_field_sort_order`. This
    change removed all references to that table.

---
+++ src/main/resources/db/postgresql/V1_56__4202_RemoveExtractFieldSortOrderTable.sql
@@ -0,0 +1,3 @@
+SET search_path TO info;
+
+DROP TABLE IF EXISTS extract_field_sort_order;
--- src/test/java/uk/gov/digital/ho/hocs/info/api/ExtractServiceTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/info/api/ExtractServiceTest.java
@@ -5,12 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import uk.gov.digital.ho.hocs.info.domain.model.ExtractFieldSortOrder;
 import uk.gov.digital.ho.hocs.info.domain.model.Field;
-import uk.gov.digital.ho.hocs.info.domain.repository.ExtractFieldSortOrderRepository;
 import uk.gov.digital.ho.hocs.info.security.AccessLevel;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -20,16 +18,11 @@ import static org.mockito.Mockito.*;
 public class ExtractServiceTest {
 
     @Mock
-    private ExtractFieldSortOrderRepository extractFieldSortOrderRepository;
-    @Mock
     private SchemaService schemaService;
 
     private ExtractService service;
 
     private static final String CASE_TYPE = "CASE_TYPE";
-    private static final String FIELD_EXT_ONLY_1 = "FieldExt1";
-    private static final String FIELD_EXT_ONLY_2 = "FieldExt2";
-    private static final String FIELD_EXT_ONLY_3 = "FieldExt3";
     private static final String FIELD_1 = "Field1";
     private static final String FIELD_2 = "Field2";
     private static final String FIELD_3 = "Field3";
@@ -39,115 +32,41 @@ public class ExtractServiceTest {
 
     @Before
     public void setup() {
-        service = new ExtractService(extractFieldSortOrderRepository, schemaService);
+        service = new ExtractService(schemaService);
         mockFields();
     }
 
     @Test
-    public void getAllReportingFieldsForCaseType_noSort() {
+    public void getAllReportingFieldsForCaseType() {
         List<String> results = service.getAllReportingFieldsForCaseType(CASE_TYPE);
 
-        assertThat(results.size()).isEqualTo(9);
-        assertThat(results.get(0)).isEqualTo(FIELD_EXT_ONLY_1);
-        assertThat(results.get(1)).isEqualTo(FIELD_EXT_ONLY_2);
-        assertThat(results.get(2)).isEqualTo(FIELD_EXT_ONLY_3);
-        assertThat(results.get(3)).isEqualTo(FIELD_1);
-        assertThat(results.get(4)).isEqualTo(FIELD_2);
-        assertThat(results.get(5)).isEqualTo(FIELD_3);
-        assertThat(results.get(6)).isEqualTo(FIELD_4);
-        assertThat(results.get(7)).isEqualTo(FIELD_5);
-        assertThat(results.get(8)).isEqualTo(FIELD_6);
+        assertThat(results.size()).isEqualTo(6);
+        assertThat(results.get(0)).isEqualTo(FIELD_1);
+        assertThat(results.get(1)).isEqualTo(FIELD_2);
+        assertThat(results.get(2)).isEqualTo(FIELD_3);
+        assertThat(results.get(3)).isEqualTo(FIELD_4);
+        assertThat(results.get(4)).isEqualTo(FIELD_5);
+        assertThat(results.get(5)).isEqualTo(FIELD_6);
 
         verify(schemaService).getExtractOnlyFields();
         verify(schemaService).getAllReportingFieldsForCaseType(CASE_TYPE);
-        verify(extractFieldSortOrderRepository).findAllByCaseTypeOrderBySortOrder(CASE_TYPE);
-        verifyNoMoreInteractions(schemaService, extractFieldSortOrderRepository);
-    }
-
-    @Test
-    public void getAllReportingFieldsForCaseType_partialSort() {
-        List<ExtractFieldSortOrder> testSortOrder = new ArrayList<>();
-        testSortOrder.add(buildSortOrder(FIELD_3, 1L));
-        testSortOrder.add(buildSortOrder(FIELD_4, 2L));
-        testSortOrder.add(buildSortOrder(FIELD_5, 3L));
-        when(extractFieldSortOrderRepository.findAllByCaseTypeOrderBySortOrder(CASE_TYPE)).thenReturn(testSortOrder);
-
-        List<String> results = service.getAllReportingFieldsForCaseType(CASE_TYPE);
-
-        assertThat(results.size()).isEqualTo(9);
-        assertThat(results.get(0)).isEqualTo(FIELD_3);
-        assertThat(results.get(1)).isEqualTo(FIELD_4);
-        assertThat(results.get(2)).isEqualTo(FIELD_5);
-        assertThat(results.get(3)).isEqualTo(FIELD_EXT_ONLY_1);
-        assertThat(results.get(4)).isEqualTo(FIELD_EXT_ONLY_2);
-        assertThat(results.get(5)).isEqualTo(FIELD_EXT_ONLY_3);
-        assertThat(results.get(6)).isEqualTo(FIELD_1);
-        assertThat(results.get(7)).isEqualTo(FIELD_2);
-        assertThat(results.get(8)).isEqualTo(FIELD_6);
-
-        verify(schemaService).getExtractOnlyFields();
-        verify(schemaService).getAllReportingFieldsForCaseType(CASE_TYPE);
-        verify(extractFieldSortOrderRepository).findAllByCaseTypeOrderBySortOrder(CASE_TYPE);
-        verifyNoMoreInteractions(schemaService, extractFieldSortOrderRepository);
-    }
-
-    @Test
-    public void getAllReportingFieldsForCaseType_completeSort() {
-        List<ExtractFieldSortOrder> testSortOrder = new ArrayList<>();
-
-        testSortOrder.add(buildSortOrder(FIELD_6, 1L));
-        testSortOrder.add(buildSortOrder(FIELD_5, 2L));
-        testSortOrder.add(buildSortOrder(FIELD_4, 3L));
-        testSortOrder.add(buildSortOrder(FIELD_3, 4L));
-        testSortOrder.add(buildSortOrder(FIELD_2, 5L));
-        testSortOrder.add(buildSortOrder(FIELD_1, 6L));
-        testSortOrder.add(buildSortOrder(FIELD_EXT_ONLY_3, 7L));
-        testSortOrder.add(buildSortOrder(FIELD_EXT_ONLY_2, 8L));
-        testSortOrder.add(buildSortOrder(FIELD_EXT_ONLY_1, 9L));
-        when(extractFieldSortOrderRepository.findAllByCaseTypeOrderBySortOrder(CASE_TYPE)).thenReturn(testSortOrder);
-
-        List<String> results = service.getAllReportingFieldsForCaseType(CASE_TYPE);
-
-        assertThat(results.size()).isEqualTo(9);
-        assertThat(results.get(0)).isEqualTo(FIELD_6);
-        assertThat(results.get(1)).isEqualTo(FIELD_5);
-        assertThat(results.get(2)).isEqualTo(FIELD_4);
-        assertThat(results.get(3)).isEqualTo(FIELD_3);
-        assertThat(results.get(4)).isEqualTo(FIELD_2);
-        assertThat(results.get(5)).isEqualTo(FIELD_1);
-        assertThat(results.get(6)).isEqualTo(FIELD_EXT_ONLY_3);
-        assertThat(results.get(7)).isEqualTo(FIELD_EXT_ONLY_2);
-        assertThat(results.get(8)).isEqualTo(FIELD_EXT_ONLY_1);
-
-        verify(schemaService).getExtractOnlyFields();
-        verify(schemaService).getAllReportingFieldsForCaseType(CASE_TYPE);
-        verify(extractFieldSortOrderRepository).findAllByCaseTypeOrderBySortOrder(CASE_TYPE);
-        verifyNoMoreInteractions(schemaService, extractFieldSortOrderRepository);
     }
 
     private void mockFields() {
-        List<Field> extractOnlyFields = new ArrayList<>();
-        extractOnlyFields.add(buildTestField(1L, FIELD_EXT_ONLY_1));
-        extractOnlyFields.add(buildTestField(2L, FIELD_EXT_ONLY_2));
-        extractOnlyFields.add(buildTestField(3L, FIELD_EXT_ONLY_3));
+        List<Field> fields = List.of(
+                buildTestField(1L, FIELD_1),
+                buildTestField(3L, FIELD_3),
+                buildTestField(2L, FIELD_2),
+                buildTestField(5L, FIELD_5),
+                buildTestField(4L, FIELD_4),
+                buildTestField(6L, FIELD_6)
+        );
 
-        List<Field> fields = new ArrayList<>();
-        fields.add(buildTestField(4L, FIELD_1));
-        fields.add(buildTestField(5L, FIELD_2));
-        fields.add(buildTestField(6L, FIELD_3));
-        fields.add(buildTestField(7L, FIELD_4));
-        fields.add(buildTestField(8L, FIELD_5));
-        fields.add(buildTestField(9L, FIELD_6));
-
-        when(schemaService.getExtractOnlyFields()).thenReturn(extractOnlyFields);
+        when(schemaService.getExtractOnlyFields()).thenReturn(Collections.emptyList());
         when(schemaService.getAllReportingFieldsForCaseType(CASE_TYPE)).thenReturn(fields.stream());
     }
 
     private Field buildTestField(Long id, String fieldName) {
         return new Field(id, null, "", fieldName, "", "", "", true, true, true, AccessLevel.READ, null);
-    }
-
-    private ExtractFieldSortOrder buildSortOrder(String fieldName, Long sortOrder) {
-        return new ExtractFieldSortOrder(null, CASE_TYPE, fieldName, sortOrder);
     }
 }
