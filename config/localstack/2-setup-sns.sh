8108a46a59392214f337b2e517dacf160f72de01
Stuart Whitlam <stuart.whitlam1@digital.homeoffice.gov.uk>
2022-06-13 17:12:25 +0100

    Update to version localstack 0.14

--- config/localstack/2-setup-sns.sh
+++ config/localstack/2-setup-sns.sh
@@ -6,9 +6,9 @@ export AWS_SECRET_ACCESS_KEY=UNSET
 export AWS_DEFAULT_REGION=eu-west-2
 
 ## make sure that localstack is running in the pipeline
-until curl http://localstack:4566/health --silent | grep -q "running"; do
+until curl http://localstack:4566/health --silent | grep -q "\"sns\": \"available\""; do
    sleep 5
    echo "Waiting for LocalStack to be ready..."
 done
 
-aws sns --endpoint-url=http://localstack:4566 create-topic --name hocs-audit-topic
+awslocal sns create-topic --name hocs-audit-topic
