4078e4261c668554b00a37415a692c5bdef20765
Stuart Whitlam <stuart.whitlam1@digital.homeoffice.gov.uk>
2022-06-15 13:21:04 +0100

    Update localstack to version 0.14 (#539)
    * Update to version localstack 0.14
    
    * update localstack in drone config

--- config/localstack/1-setup-sqs.sh
+++ config/localstack/1-setup-sqs.sh
@@ -6,10 +6,10 @@ export AWS_SECRET_ACCESS_KEY=UNSET
 export AWS_DEFAULT_REGION=eu-west-2
 
 ## make sure that localstack is running in the pipeline
-until curl http://localstack:4566/health --silent | grep -q "running"; do
+until curl http://localstack:4566/health --silent | grep -q "\"sqs\": \"available\""; do
    sleep 5
    echo "Waiting for LocalStack to be ready..."
 done
 
-aws sqs --endpoint-url=http://localstack:4566 create-queue --queue-name notify-queue-dlq
-aws sqs --endpoint-url=http://localstack:4566 create-queue --queue-name notify-queue --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:notify-queue-dlq\",\"maxReceiveCount\":2}"}'
+awslocal sqs create-queue --queue-name notify-queue-dlq
+awslocal sqs create-queue --queue-name notify-queue --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:notify-queue-dlq\",\"maxReceiveCount\":2}"}'
