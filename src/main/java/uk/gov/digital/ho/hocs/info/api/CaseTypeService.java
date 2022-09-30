e6e3aeda5a6191812cc1be4e8cf08a3786586c2b
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2022-09-30 13:41:35 +0100

    HOCS-5363: remove deprecated case type tables

--- src/main/java/uk/gov/digital/ho/hocs/info/api/CaseTypeService.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/api/CaseTypeService.java
@@ -6,16 +6,22 @@ import org.springframework.stereotype.Service;
 import uk.gov.digital.ho.hocs.info.api.dto.CaseTypeActionDto;
 import uk.gov.digital.ho.hocs.info.api.dto.CreateCaseTypeDto;
 import uk.gov.digital.ho.hocs.info.domain.exception.ApplicationExceptions;
-import uk.gov.digital.ho.hocs.info.domain.model.*;
+import uk.gov.digital.ho.hocs.info.domain.model.CaseType;
+import uk.gov.digital.ho.hocs.info.domain.model.CaseTypeAction;
+import uk.gov.digital.ho.hocs.info.domain.model.DocumentTag;
+import uk.gov.digital.ho.hocs.info.domain.model.ExemptionDate;
 import uk.gov.digital.ho.hocs.info.domain.repository.CaseActionTypeRepository;
-import uk.gov.digital.ho.hocs.info.domain.repository.CaseTabRepository;
 import uk.gov.digital.ho.hocs.info.domain.repository.CaseTypeRepository;
 import uk.gov.digital.ho.hocs.info.domain.repository.DocumentTagRepository;
 import uk.gov.digital.ho.hocs.info.domain.repository.HolidayDateRepository;
 import uk.gov.digital.ho.hocs.info.security.UserPermissionsService;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,37 +30,25 @@ public class CaseTypeService {
 
     private final CaseTypeRepository caseTypeRepository;
 
-    private final CaseTabRepository caseTabRepository;
-
     private final DocumentTagRepository documentTagRepository;
 
     private final HolidayDateRepository holidayDateRepository;
 
     private final CaseActionTypeRepository caseActionTypeRepository;
 
-    private final StageTypeService stageTypeService;
-
     private final UserPermissionsService userPermissionsService;
-
-    private final LocalDateWrapper localDateWrapper;
 
     @Autowired
     public CaseTypeService(CaseTypeRepository caseTypeRepository,
-                           CaseTabRepository caseTabRepository,
                            DocumentTagRepository documentTagRepository,
                            HolidayDateRepository holidayDateRepository,
                            CaseActionTypeRepository caseActionTypeRepository,
-                           StageTypeService stageTypeService,
-                           UserPermissionsService userPermissionsService,
-                           LocalDateWrapper localDateWrapper) {
+                           UserPermissionsService userPermissionsService) {
         this.caseTypeRepository = caseTypeRepository;
-        this.caseTabRepository = caseTabRepository;
         this.documentTagRepository = documentTagRepository;
         this.holidayDateRepository = holidayDateRepository;
         this.caseActionTypeRepository = caseActionTypeRepository;
-        this.stageTypeService = stageTypeService;
         this.userPermissionsService = userPermissionsService;
-        this.localDateWrapper = localDateWrapper;
     }
 
     Set<CaseType> getAllCaseTypes(Boolean addCaseTypeWithPreviousType) {
@@ -112,19 +106,6 @@ public class CaseTypeService {
             return caseType;
         } else {
             throw new ApplicationExceptions.EntityNotFoundException("CaseType for type %s was not found", type);
-        }
-    }
-
-    @Deprecated(forRemoval = true)
-    CaseConfig getCaseConfig(String type) {
-        log.debug("Getting CaseConfig for type {}", type);
-        List<CaseTab> tabs = caseTabRepository.findTabsByType(type);
-
-        if (!tabs.isEmpty()) {
-            log.info("Got CaseConfig for type {}", type);
-            return new CaseConfig(type, tabs);
-        } else {
-            throw new ApplicationExceptions.EntityNotFoundException("CaseConfig for type %s was not found", type);
         }
     }
 
