2a92619f8a853abe846c9f18e9cd7460211115c6
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-12-14 09:41:40 +0000

    HOCS-5983: Only refresh countries list with a profile enabled

--- src/main/java/uk/gov/digital/ho/hocs/info/startup/RefreshCountries.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/startup/RefreshCountries.java
@@ -3,12 +3,14 @@ package uk.gov.digital.ho.hocs.info.startup;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import uk.gov.digital.ho.hocs.info.api.CountryService;
 
 @Service
 @Slf4j
+@Profile({ "refreshCountries" })
 public class RefreshCountries {
 
     private final CountryService countryService;
