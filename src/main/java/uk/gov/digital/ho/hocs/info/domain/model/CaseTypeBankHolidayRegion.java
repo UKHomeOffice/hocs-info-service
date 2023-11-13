4dc7b9d4ee0de7c659a4c1259f9a8c94d6b17d80
vinothon84 <vinothon84@gmail.com>
2023-11-13 15:06:39 +0000

    HOCS-6820:Build changes to fix compilation errors

--- src/main/java/uk/gov/digital/ho/hocs/info/domain/model/CaseTypeBankHolidayRegion.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/domain/model/CaseTypeBankHolidayRegion.java
@@ -3,6 +3,7 @@ package uk.gov.digital.ho.hocs.info.domain.model;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.Type;
 //import org.hibernate.annotations.TypeDef;
 import uk.gov.digital.ho.hocs.info.domain.model.enums.BankHolidayRegion;
@@ -14,6 +15,7 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 //@TypeDef(name = "pgsql_enum", typeClass = org.hibernate.type.EnumType.class)
+@Convert(attributeName = "pgsql_enum", converter = org.hibernate.type.EnumType.class)
 @Table(name = "case_type_bank_holiday_region")
 public class CaseTypeBankHolidayRegion {
 
@@ -28,9 +30,10 @@ public class CaseTypeBankHolidayRegion {
     private UUID caseTypeUuid;
 
     @Getter
-    @Enumerated(EnumType.STRING)
+    //@Enumerated(EnumType.STRING)
     @Column(name = "region")
     //@Type(type = "pgsql_enum")
+    @Enumerated(EnumType.ORDINAL)
     private BankHolidayRegion region;
 
     public CaseTypeBankHolidayRegion(UUID caseTypeUuid, String region) {
