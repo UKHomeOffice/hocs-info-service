d1bd7e0fdf65aa28520a93e8f10a03df4e4f7173
Jeff Horton <jeff.horton@digital.homeoffice.gov.uk>
2026-01-08 11:51:57 +0000

    fix missing updated StringUtils version

--- src/main/java/uk/gov/digital/ho/hocs/info/api/EntityService.java
+++ src/main/java/uk/gov/digital/ho/hocs/info/api/EntityService.java
@@ -1,7 +1,7 @@
 package uk.gov.digital.ho.hocs.info.api;
 
-import io.micrometer.common.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
